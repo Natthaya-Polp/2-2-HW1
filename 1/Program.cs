c7ddb28f66372e56b1ba383a2a9b5aab417e184a
Natthaya-Polp <natthayacud56@gmail.com>
2023-02-06 22:41:42 +0700

    finish

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(Recursive(x));
+    }
+
+    static double Recursive(double x)
+    {
+        if ( Math.Pow( x , 2 ) <= 4 )
+        {
+            return 2;
+        }
+        else
+        {
+            return 1 + Recursive( x - ( (4 * Math.Abs(x)) / x ));
+        }
+    }
+}
